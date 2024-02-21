5b84baea71e141152f9cfff015cffca59089e332
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-21 11:36:41 +0100

    read langauge from url and set content accordinlgy

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
-import {useState} from "preact/hooks"
+import {useState, useEffect} from "preact/hooks"
+import { getCurrentUrl } from 'preact-router';
+
 import "../index.css"
 
 const Home = () => {
 
     const [language, setLanguage] = useState("en")
+
+    useEffect(() => {
+        // Parse the current URL to find the language parameter
+        const url = new URL(window.location.href);
+        const pathSegments = url.pathname.split('/').filter(Boolean); // filter(Boolean) removes empty strings from the array
+        if (pathSegments.length > 0) {
+            // Assuming the language code is the first segment after the first '/'
+            const langParam = pathSegments[0];
+            setLanguage(langParam);
+        }
+    }, [getCurrentUrl()]); // Depend on the current URL
 
     return(
         <div>
