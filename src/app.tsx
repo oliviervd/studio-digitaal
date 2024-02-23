efa576487f97f930f3112967900727619b06fe4f
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-23 11:41:54 +0100

    add redirect from "/" to "/en"

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,22 @@
 import Home from "./pages/home";
-import Router from "preact-router";
+import {Router, route} from "preact-router";
 import { render } from 'preact';
+import { useEffect } from 'preact/hooks';
+
+
 
 const App = () => {
 
+    useEffect(() => {
+        const currentPath = window.location.pathname;
+        if (currentPath === '/') {
+            route('/en', true); // Redirect to '/en'
+        }
+    }, []); // Empty dependency array means this effect runs once on mount
+
   return (
     <Router>
-      <Home path={"/:lang"}/>
+        <Home path={"/:lang"}/>
     </Router>
   )
 }
