978c86f39dc97c3617155a9f6cb0a3fc94dbf60a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-03-25 16:14:35 +0100

    add component that renders blocks from PayloadCMS

---
+++ src/components/block.tsx
@@ -0,0 +1,30 @@
+import {ApiResponse} from "./ApiResponse";
+import serialize from "../utils/serialize";
+
+const Block = (props) => {
+    // if title parse as title
+    if (props.data.type === "title") {
+        return(
+            <h1>{props.data.text}</h1>
+        )
+    }
+
+    // if endpoint use to generate api response
+    if (props.data.type === "endpoint") {
+        return(
+            <ApiResponse endpoint={props.data.text}/>
+        )
+    }
+
+    // if richtext
+    if (props.data.blockType === "RichTextBlock") {
+        console.log(props.data.richText)
+        let text = serialize(props.data.richText[0].children)
+        console.log(text)
+        return(
+            <div>{text}</div>
+        )
+    }
+
+}
+export default Block
