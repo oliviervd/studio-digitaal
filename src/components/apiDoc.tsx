6ff47efc2ac0d3f30f1daa4c6cb0fb9c5a5f6473
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-03-25 16:14:52 +0100

    update apiDOC page

--- src/components/apiDoc.tsx
+++ src/components/apiDoc.tsx
@@ -1,20 +1,18 @@
-// displays API documenteation
-// includes API response components for working examples.
+import Block from "./block";
 
-import {ApiResponse} from "./ApiResponse";
-import {FunctionComponent} from "preact";
+const ApiDoc = (props) => {
+    if (props.apiPage) {
+        return(
+            <div className={"api-doc"}>
+                {props.apiPage.layout.map((e)=>{
+                    return(
+                        <Block data={e}/>
+                    )
+                })}
+            </div>
+        )
 
-interface ApiDocProps {
-    endpoint: string;
-    description: string
-}
-const ApiDoc : FunctionComponent<ApiDocProps> = ({endpoint, description}) => {
-    return(
-        <div className={"api-doc"}>
-            <p>{description}</p>
-            <ApiResponse endpoint={endpoint}/>
-        </div>
-    )
+    }
 }
 
 
