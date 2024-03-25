992bb2048765c18ffdeb8a9e2901f78928ecfbc1
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-03-25 16:14:45 +0100

    update collectionAPI page

--- src/pages/CollectionAPI.tsx
+++ src/pages/CollectionAPI.tsx
@@ -1,13 +1,32 @@
-import {useState} from "preact/hooks";
-const ApiDocs = (props) => {
+import {useState, useEffect} from "preact/hooks";
+import {fetchPayload} from "../utils/fetchPayload";
+import "../styles/api-docs.css"
 
-    const [open, setOpen] = useState(false);
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Sidebar from "../components/Sidebar";
+import ApiDoc from "../components/apiDoc";
+
+const ApiDocs = (props) => {
+    const baseURI:string = "https://p01--admin-cms--qbt6mytl828m.code.run";
+    const [apiPages, setApiPages] = useState([])
+    const [apiPage, setApiPage] = useState(apiPages[0])
+    // parse data from payload
+
+    useEffect(() => {
+        fetchPayload(baseURI, "apiDoc", 10, props.language).then((data)=>{
+            setApiPages(data.docs);
+        })
+    }, []);
 
     return(
         <div>
-            <section >
-                <p>test</p>
+            <Header/>
+            <section className={"api-doc__container"}>
+                <Sidebar apiPages={apiPages} setApiPage={setApiPage}/>
+                <ApiDoc apiPage={apiPage}/>
             </section>
+            <Footer/>
         </div>
     )
 
