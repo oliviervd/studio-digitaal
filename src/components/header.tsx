9744a4a67722571e1a15ba2ef4d79bb22fb45c68
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-06-07 12:07:30 +0200

    update header (logo; navigate to home)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,24 @@ import "../styles/navigation.css"
 import logo from "../assets/Pixel-Logo-41-frames-transparent.gif"
 import FontChanger from "./fontChanger";
 import CalculateSize from "./fetchSize";
+import {route} from "preact-router"
 
 const Header = ({changeLang, openLogoDesc, logoDesc, handleFontChange}) => {
     // todo: add responsible design for mobile.
 
-    return(
+    function logoAction() {
+        if (openLogoDesc) {
+            openLogoDesc(!logoDesc);
+        }
+        route("")
+    }
 
+    return(
             <header className={"header__desktop"}>
                 <div className={"header__logo-container"}>
                     <img
                         src={logo} alt={"logo of studio digitaal"}
-                        onClick={() => openLogoDesc(!logoDesc)}
+                        onClick={() => logoAction()}
                     />
                 </div>
 
