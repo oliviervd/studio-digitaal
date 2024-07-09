cc95cd92de65c14f3afe0cd0b9b695e45fa1328d
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-07-09 10:48:11 +0200

    restyle nested content (padding)

--- src/pages/NestedContent.tsx
+++ src/pages/NestedContent.tsx
@@ -1,22 +1,35 @@
 import {useLanguage} from "../utils/languageProvider";
 import serialize from "../utils/serialize";
+import {useEffect} from "preact/hooks";
 
 const NestedContent = ({projects}) => {
     const {language, setLanguage} = useLanguage()
 
+    useEffect(() => {
+        // fetch ID
+        const id = window.location.hash.substring(1)
+
+        const elem = document.getElementById(id);
+        if (elem) {
+            //console.log(id)
+            //console.log(elem)
+        } else  {
+            //console.log("no such element")
+        }
+
+    }, []);
+
     return(
         <section style={{borderLeft: "2px solid blue"}} className={"L2-container"}>
             {projects && projects.map((project, index) => {
-                if (project.article.subProjects[0] && project.article.subProjects[0].project !== null){
-                    console.log(project.article.subProjects)
-                }
+                console.log(project.article.path)
                 return(
-                    <details key={project._id}>
+                    <details id={project.article.path}>
                         <summary>
                             {project.article.projectTitle}
                         </summary>
                         {project.article.projectDescription &&
-                            <div style={{borderLeft: "2px solid pink", paddingLeft: "20px"}}>
+                            <div style={{borderLeft: "2px solid pink", paddingLeft: "30px"}}>
                                 {project.article.heroImage &&
                                     <details>
                                         <summary>image</summary>
@@ -28,11 +41,10 @@ const NestedContent = ({projects}) => {
                                 }
                                 <p>{serialize(project.article.projectDescription)}</p>
                                 {project.article.subProjects[0] && project.article.subProjects[0].project !== null && project.article.subProjects.map((project)=> {
-                                    console.log(project.project)
                                     return (
                                         <details>
                                             <summary>{project.project.projectTitle}</summary>
-                                            <div style={{borderLeft: "2px solid pink", paddingLeft: "20px"}}>
+                                            <div style={{borderLeft: "2px solid pink", paddingLeft: "30px"}}>
                                                 <p>{serialize(project.project.projectDescription)}</p>
                                             </div>
                                         </details>
